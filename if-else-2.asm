d8f03a88a74c23759bb948c8945143714750a354
Beko K <101501854+amaym0nn@users.noreply.github.com>
2022-12-10 17:31:18 +0300

    Add files via upload

---
+++ if-else-2.asm
@@ -0,0 +1,41 @@
+; iki sayinin birbirinden buyuk veya kucuk oldugunu belirten assembly uygulamasi
+
+
+section .data
+	msg1: 	db "nm1 > nm2", 10
+	str1: equ $ - msg1
+
+	msg2:	db "nm2 > nm1", 10
+	str2: equ $ - msg2
+	
+section .text
+	global _start
+
+_start:
+	mov edi,21
+	mov esi,24
+	cmp edi,esi	
+	jge jumpGreater ; if (nm1 > nm2)
+	jl jumpLess	; if (nm2 > nm1)	
+
+jumpGreater:
+	mov eax,4
+	mov ebx,1
+	mov ecx,msg1
+	mov edx,str1
+	int 0x80
+	
+	mov eax,1
+	mov ebx,0
+	int 0x80
+
+jumpLess:
+	mov eax,4
+	mov ebx,1
+	mov ecx,msg2
+	mov edx,str2
+	int 0x80
+	
+	mov eax,1
+	mov ebx,0
+	int 0x80
