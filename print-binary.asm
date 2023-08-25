62f80d97e57079b76072e7c77cdeb6241ff8374f
Berkay <devtborne@gmail.com>
2023-08-25 14:01:27 +0300

    Update print-binary.asm

--- print-binary.asm
+++ print-binary.asm
@@ -33,18 +33,24 @@ _exit:
 	int 0x80
 
 _nextBinary:
+	; In this section, bit 1 is advanced when 0 is detected in the binary input
 	inc ecx
 _loop:
+	; in this section, if the end of the string is reached, the program is terminated
 	cmp byte [edi + ecx],0
 	je _exit
 
+	; Important Section. If the user enters a number greater than 1 in the input, it is advanced by 1 bit to skip this value.
 	cmp byte [edi + ecx],'1'
 	jg _nextBinary
 
+	; In this section, if there is a 0 in the input, 1 bit is advanced.
 	cmp byte [edi + ecx],'0'
 	je _nextBinary
 
+	; The program is directed to the _printOneBinary function to print the value 1 on the screen
 	call _printOneBinary
+
 	mov ecx,[loopCounter]
 	mov dl,byte [currentBinaryLocation]
 	mov byte [edi + ecx],dl
